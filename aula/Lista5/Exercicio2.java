70731e84c0aee7f683664fc59d56d69833335ba0
Giovanna Regina <101228393+kuinagi@users.noreply.github.com>
2025-10-10 01:10:36 +0000

    Exercicio 2

--- aula/Lista5/Exercicio2.java
+++ aula/Lista5/Exercicio2.java
@@ -1,1 +1,22 @@
-import java.util.Scanner
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Insira as notas: ");
+        double soma = sc.nextDouble();
+        soma += sc.nextDouble();
+        soma += sc.nextDouble();
+        soma += sc.nextDouble();
+
+        System.out.printf("Insira a frequencia: ");
+        int frequencia = sc.nextInt();
+        
+        System.out.printf("A media é: %.2f %s %n", soma/4, (frequencia > 75) && (soma >= 6) ? "Aprovado" : "Reprovado");
+
+
+        sc.close();
+    }
+}
