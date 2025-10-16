52f27f97383e1f133feaee20c7eab64e08466337
Giovanna Regina <101228393+kuinagi@users.noreply.github.com>
2025-10-16 14:09:44 +0000

    Exerc 4

--- aula/Lista5/Exercicio4.java
+++ aula/Lista5/Exercicio4.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Exercicio4 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um ano: ");
+        int ano = sc.nextInt();
+
+        boolean bissexto = (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+
+        System.out.printf("O ano %d é bissexto? %b %n", ano, bissexto);
+
+        sc.close();
+    }
 }
